d689c30611baee79c02107bb6c185b892a3782ee
atharvWaste <atharvawaste16@gmail.com>
2025-10-30 14:06:33 +0530

    Add files via upload

---
+++ index.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+int calFun(int a, int b, char c);
+void main() {
+   for(int i=1; i++; i>0)  
+      {
+      int a;
+      int b;
+      char c;
+
+    // to take input from the user
+    printf("Enter your first number");
+    scanf("%d", &a);
+     printf("Enter your opration");
+    scanf(" %c", &c);
+     printf("Enter your another number");
+    scanf("%d", &b);
+  calFun(int a, int b, char c);
+  
+}if(c=='+'){
+        printf("Enter your another number");
+    }else if (c=='-')
+    {
+       printf("Enter your another number");
+    }else if (c=='*')
+    {
+      
+        printf("Enter your another number");
+    }else if (c=='/')
+    {
+       printf("Enter your another number");
+    }else
+    {
+        
+      printf("Enter your another number");
+    }
+
+int calFun(int a, int b, char c){
+    int d;
+  if(c=='+'){
+        int d = a+b;
+    }else if (c=='-')
+    {
+        int d = a+b;
+    }else if (c=='*')
+    {
+      
+        int d = a+b;
+    }else if (c=='/')
+    {
+        int d = a+b;
+    }else
+    {
+        
+       int d=0;
+    }
+}
+ return d;
+}
+
+// there should be limitation on the input to be a number only...
+// you know that this is an wrond way .. 
+// try to use the concept of return 
+// i want to use do wile loop since the infint loop is running in abow program and there by..
+//  i also what to include the concept of pointers b4 i put this on github account..
